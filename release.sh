bf1a7e3023fc2b7fb7f9c218a046ebb32fbb196a
Omar Zorob <omar.zorob@yahoo.com>
2021-02-18 16:58:13 +0000

    SAGE-693 Initial Waggle Internet Share

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+
+mkdir -p /tmp/reg
+
+# Build the local internet share debian package
+BASEDIR=/tmp/reg
+NAME=wagggle-internet-share
+ARCH=all
+
+mkdir -p ${BASEDIR}/DEBIAN
+cat > ${BASEDIR}/DEBIAN/control <<EOL
+Package: ${NAME}
+Version: ${VERSION_LONG}
+Maintainer: sagecontinuum.org
+Description: Starting Waggle Local Internet Service
+Architecture: ${ARCH}
+Priority: optional
+EOL
+
+cp -p deb/install/postinst ${BASEDIR}/DEBIAN/
+cp -p deb/install/prerm ${BASEDIR}/DEBIAN/
+
+cp -pr ROOTFS/* ${BASEDIR}/
+
+dpkg-deb --root-owner-group --build ${BASEDIR} "${NAME}_${VERSION_SHORT}_${ARCH}.deb"
+mv *.deb /output/

9ef4149750fc4505534b782ec7ebdde33302967e
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-03 21:21:26 -0500

    SAGE-1354: remove 'Sage' reference and use latest deb-builder

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 docker run --rm \
   -e NAME="waggle-internet-share" \
   -e DESCRIPTION="Waggle Internet Sharing Service" \
-  -e "MAINTAINER=sagecontinuum.org" \
   -v "$PWD:/repo" \
-  waggle/waggle-deb-builder:0.2.0
+  waggle/waggle-deb-builder:latest

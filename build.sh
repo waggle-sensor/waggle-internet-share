7d8b827e0f399f3a59622b73a9506c507fac027e
Joe Swantek <joseph.swantek@northwestern.edu>
2022-06-08 12:55:10 -0500

    SAGE-1354: add `waggle-common-tools` dependency
    This is for the `waggle-get-config` tool.

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 docker run --rm \
   -e NAME="waggle-internet-share" \
   -e DESCRIPTION="Waggle Internet Sharing Service" \
+  -e "DEPENDS=waggle-common-tools (>= 0.2.0)" \
   -v "$PWD:/repo" \
   waggle/waggle-deb-builder:latest
